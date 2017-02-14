a72fe653b7612f417e821913302039581aa143b8
Di Gangi Stefania <digst@usz.ch>
2017-02-14 09:15:23 +0100

    comm

--- fib.r
+++ fib.r
@@ -1,4 +1,4 @@
-data=read.table("creditcard.txt") 
+data=read.table("test.txt") 
 lcases= log(data[,2])
 data=cbind(data,lcases)
 colnames(data)=c("income","cases","CrCards","lcases")
@@ -15,42 +15,41 @@ fitted(log.fit)
 
 
 library(changepoint)
-set.seed(10)
-m.data=c(rnorm(100,0,1),rnorm(100,1,1),rnorm(100,0,1),rnorm(100,0.2,1))
-ts.plot(m.data,xlab="Index")
-m.pelt=cpt.mean(m.data,method="PELT")
+#set.seed(10)
+#m.data=c(rnorm(100,0,1),rnorm(100,1,1),rnorm(100,0,1),rnorm(100,0.2,1))
+ts.plot(data[,3],xlab="Index")
+m.pelt=cpt.mean(data[,3],method="PELT")
 plot(m.pelt,type="l",cpt.col="blue",xlab="Index",cpt.width=4)
 cpts(m.pelt)
-m.binseg=cpt.mean(m.data,method="BinSeg")
+m.binseg=cpt.mean(data[,3],method="BinSeg")
 plot(m.binseg,type="l",xlab="Index",cpt.width=4)
 cpts(m.binseg)
-m.pm=cpt.mean(m.data,penalty="Manual",pen.value="1.5*log(n)",method="PELT")
+m.pm=cpt.mean(data[,3],penalty="Manual",pen.value="1.5*log(n)",method="PELT")
 plot(m.pm,type="l",cpt.col="blue",xlab="Index",cpt.width=4)
 cpts(m.pm)
 
 #bayesian change point
 library(bcp)
-plot(bcp(m.data))
+plot(bcp(data[,3]))
 
 #structural change
 library(strucchange)
-ocus.n <- efp(m.data ~ 1, type = "OLS-CUSUM")
+ocus.n <- efp(data[,3] ~ 1, type = "OLS-CUSUM")
 plot(ocus.n)
 
-fs.n <- Fstats(m.data ~ 1)
+fs.n <- Fstats(data[,3] ~ 1)
 plot(fs.n)
 
-bp.n <- breakpoints(m.data ~ 1)
+bp.n <- breakpoints(data[,3] ~ 1)
 plot(bp.n)
-fm0.n <- lm(m.data ~ 1)
+fm0.n <- lm(data[,3] ~ 1)
 coef(fm0.n)
-c<-2
 
 
 n.fac <- breakfactor(bp.n)
-fm1.n <- lm(m.data ~ n.fac - 1)
+fm1.n <- lm(data[,3] ~ n.fac - 1)
 coef(fm1.n)
 
-ts.plot(m.data,xlab="Index")
+ts.plot(data[,3],xlab="Index")
 lines(fitted(fm0.n))
 lines(fitted(fm1.n))
