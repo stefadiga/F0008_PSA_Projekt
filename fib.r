ab75a52d026658e90c9b47bba01aed5b90884dff
Di Gangi Stefania <digst@usz.ch>
2017-02-14 08:39:35 +0100

    pois

--- fib.r
+++ fib.r
@@ -1,3 +1,18 @@
+data=read.table("creditcard.txt") 
+lcases= log(data[,2])
+data=cbind(data,lcases)
+colnames(data)=c("income","cases","CrCards","lcases")
+data
+
+## Fit the data with poisson linear model with offset=lcases
+
+log.fit=glm(CrCards~income+offset(lcases),family=poisson,data=data)
+summary(log.fit)
+
+## Fitted Values under poisson linear model
+
+fitted(log.fit)
+
 
 library(changepoint)
 set.seed(10)
